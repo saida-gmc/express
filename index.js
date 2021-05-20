47eb3ec82d3330e3f6b9a2880530b7fd35c3e87a
saida-gmc <81155421+saida-gmc@users.noreply.github.com>
2021-05-20 23:21:52 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const app = express();
+
+const homeRoute = require("./routes/HomePage");
+const servicesRoute = require("./routes/OurServices");
+const contactRoute = require("./routes/ContactUs");
+const workingTimeRoute = require("./routes/WorkingTime");
+
+app.use(express.static(path.join(__dirname, "public", "stylesheet")));
+
+let date = new Date();
+let day = date.getDay();
+let hours = date.getHours();
+if (day < 6 && hours > 9 && hours < 17) {
+  app.use(homeRoute);
+  app.use(servicesRoute);
+  app.use(contactRoute);
+} else {
+  app.use(workingTimeRoute);
+}
+app.listen(5000, () => {
+  console.log("server is running on port 5000");
+});
